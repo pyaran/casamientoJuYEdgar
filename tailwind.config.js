e425265434ff196ee4e8b383694edf4330c73ed6
Santiago Jimenez Montilla <santiagojimenezmontilla@gmail.com>
2024-08-10 16:36:49 -0300

    darkmode:false

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-undef
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 // eslint-disable-next-line no-undef
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
+  darkMode: false, // Disable dark mode
   plugins: [],
 });
